80a528770bf36425211c8e87e9fb2ae780f54251
ESSAKIRAJA <102535943+essakirajas@users.noreply.github.com>
2025-04-17 10:43:01 +0530

    Delete backend directory

--- backend/resolver/resolve.js
+++
@@ -1,9 +0,0 @@
-const { mergeResolvers } = require('@graphql-tools/merge');
-const { user } = require('./userResolver');
-const { message } = require('./messageResolver');
-const { dashboard } = require('./dashboardResolver');
-
-
-const resolvers = mergeResolvers([user, message, dashboard]);
-
-module.exports = resolvers;
