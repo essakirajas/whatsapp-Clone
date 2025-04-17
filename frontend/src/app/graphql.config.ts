8071942463885794f4794233baa06386b70f6b80
ESSAKIRAJA <102535943+essakirajas@users.noreply.github.com>
2025-04-17 10:43:14 +0530

    Delete frontend directory

--- frontend/src/app/graphql.config.ts
+++
@@ -1,2 +0,0 @@
-export const myUrl = 'http://localhost:5000/graphqlserver'; // ✅ Change for production
-export const wsUrl = 'ws://localhost:5000/graphqlserver'; // ✅ WebSocket URL for subscriptions
